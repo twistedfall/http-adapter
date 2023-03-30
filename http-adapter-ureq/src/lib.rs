3690ace3a1932b58163288103f1471714c562e58
Pro <twisted.fall@gmail.com>
2023-03-30 21:58:32 +0200

    Use client in ureq

--- http-adapter-ureq/src/lib.rs
+++ http-adapter-ureq/src/lib.rs
@@ -1,10 +1,6 @@
 use std::io::Read;
 
-use http_adapter::{
-	async_trait::async_trait,
-	HttpClientAdapter,
-	http::Request,
-};
+use http_adapter::{async_trait::async_trait, http::Request, HttpClientAdapter};
 
 #[derive(Clone, Debug)]
 pub struct UreqAdapter {
@@ -21,8 +17,8 @@ impl Default for UreqAdapter {
 }
 
 #[inline]
-fn from_request<B>(request: &Request<B>) -> Result<ureq::Request, ureq::Error> {
-	let mut out = ureq::request(request.method().as_str(), &request.uri().to_string());
+fn from_request<B>(client: &ureq::Agent, request: &Request<B>) -> Result<ureq::Request, ureq::Error> {
+	let mut out = client.request(request.method().as_str(), &request.uri().to_string());
 	for (name, value) in request.headers() {
 		if let Ok(value) = value.to_str() {
 			out = out.set(name.as_str(), value);
@@ -36,8 +32,7 @@ impl HttpClientAdapter for UreqAdapter {
 	type Error = ureq::Error;
 
 	async fn execute(&self, request: Request<Vec<u8>>) -> Result<Vec<u8>, Self::Error> {
-		let res = from_request(&request)?
-			.send_bytes(request.body())?;
+		let res = from_request(&self.client, &request)?.send_bytes(request.body())?;
 		let mut out = vec![];
 		res.into_reader().read_to_end(&mut out)?;
 		Ok(out)
